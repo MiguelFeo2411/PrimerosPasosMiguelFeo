9fe30d3199d436281450c39a67bf50032025bbd0
Miguel Feo <mfemerchan@gmail.com>
2023-04-05 20:46:41 -0500

    Crear la tabla con todos los datos, retornar lo que se mostrara en la pagina

--- src/App.js
+++ src/App.js
@@ -20,4 +20,56 @@ const CourseItem = () => {
       nombre: 'Pesebres de plastilina'
     }
   ]);
-}
+
+  const agregarCurso = (event) => {
+    event.preventDefault();
+    if (!nuevoCurso.trim() && !nuevoId.trim() ) {
+      return;
+    }
+    const [nuevoCurso, setNuevoCurso] = useState("");
+    const [nuevoId, setNuevoID] = useState("");
+    const curso = {  
+      id: nuevoId,
+      nombre: nuevoCurso };
+    setCursos([...cursos, curso]);
+    setNuevoCurso("");
+    setNuevoID("");
+  }
+
+  const eliminarCurso = (id) => {
+    setCursos(cursos.filter(curso => curso.id !== id));
+  };
+  return (
+    <div>
+      <h1>Lista de Cursos</h1>
+      <form onSubmit={agregarCurso}>
+        <label>  Nombre de la materia      <input type="text" value={nuevoCurso} onChange={(event)=> setNuevoCurso(event.target.value)} placeholder="Ingrese el nombre de la nueva materia"/></label>
+        <label>  ID de la materia     <input type="text" value={nuevoId} onChange={(event)=> setNuevoID(event.target.value)} placeholder="Ingrese el nuevo id"/></label>
+        <button type="submit">Agregar Curso</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>ID de la materia</th>
+            <th>Nombre de la materia</th>
+            <th>Eliminar materia</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cursos.map((curso) => (
+            <tr key={curso.id}>
+              <td><p>{curso.id}</p></td>
+              <td>{curso.nombre}</td>
+              <td>
+                <button onClick={() => eliminarCurso(curso.id)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+export default CourseItem;
